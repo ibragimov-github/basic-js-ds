beb0a96bb5524283904028fdec2c54f0ee0d305d
ibragimov-github <texet0598@gmail.com>
2022-05-02 19:59:01 +0300

    add: new changes

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  queueList = [];
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queueList[0]
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    let node = new ListNode(value);
+    if (!this.queueList.length) {
+      this.queueList.push(node)
+    }
+    else {
+      this.queueList[this.queueList.length -1].next = node;
+      this.queueList.push(node);
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queueList.shift().value;
   }
 }
 
 module.exports = {
   Queue
 };
+
+
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(3);
+console.log(queue.dequeue());
+console.log(queue.getUnderlyingList());
