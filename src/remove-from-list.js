c074fe446b55c094e5907e399460bc0c7d16167b
ibragimov-github <texet0598@gmail.com>
2022-05-02 18:53:48 +0300

    add: new changes

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -23,41 +23,24 @@ const { ListNode } = require('../extensions/list-node.js');
  * }
  */
 function removeKFromList(l, k) {
-  const indexOf = function(array, element) {
-    let current = array;
-    let index = 0;
-    while(current) {
-      if (current.value === element) {
-        return index;
+  let currentNode = l; 
+  let preNode = null;
+  while (currentNode) {
+    if (currentNode.value === k) {
+      if (!preNode) {
+        l = currentNode.next;
+      } else {
+        preNode.next = currentNode.next;
       }
-      current = current.next;
-      index++;
+      currentNode = l;
     }
-    return -1;
+    preNode = currentNode;
+    currentNode = currentNode.next;
   }
-  const removeAt(l, position) {
-    let current = l;
-    let position = indexOf(l, k)
-    if (position === 0) {
-      l = current.next;
-    } else {
-      let prev = null;
-      let index = 0;
-
-      while(index < position) {
-        prev = current;
-        current = current.next;
-        index++;
-      }
-      prev.next = current.next;
-    }
-    l.length --;
-    return current.value;
-  }
-  }
-  while(!indexOf(l, k)) {
-    removeAt(l, indexOf(l, k))
+  return l;
 }
+  
+
 
 module.exports = {
   removeKFromList
